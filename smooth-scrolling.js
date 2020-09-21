9ed99c217793743d30a556d41125c9742c40dacd
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-09-20 22:27:29 -0300

    Added smooth-scrolling.js and initiate the development

---
+++ smooth-scrolling.js
@@ -0,0 +1,24 @@
+class SmoothScroll {
+    constructor() {
+      /*    ## CONFIGURATION VARIABLES SECTION ##
+       *    
+       *    __ TIPS:
+       *       @1 * [Every configuration variable starts it's name with '_' character (underline).]
+       * 
+       *    __ RESUME: 
+       *    
+       *       In lines [17] to [19], there are configuration variables that allows the javascript
+       *       to identify in the HTML structure the elements that it will work on, by using CSS classes.
+       * 
+       *       According to /README.md, in order to change the default CSS classes configuration,
+       *       it's just needed to change the variable string attribution to a preferred one.
+       *    
+       */
+      const _container = '.container'
+      const _containerBody = '.container__body'
+      const _containerHitbox = '.container--hitbox'
+  
+    }
+}
+  
+new SmoothScroll()
